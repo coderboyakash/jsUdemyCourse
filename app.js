0fc8c29c73e92bb003246e53228b8a40bb631861
coderboyakash <akashchowdhury864@gmail.com>
2020-09-19 19:04:26 +0530

    till lecture 6 section 2

---
+++ app.js
@@ -0,0 +1,144 @@
+// console.log('Script file is loaded !');
+// var greeting = 'Hello JavaScript';
+// console.log({a:1,b:2});
+// console.table({a:1,b:2});
+// console.error('this is error');
+// console.warn('this is waring');
+// console.time('Hello');
+//   console.log('Script file is loaded !');
+//   console.log('Script file is loaded !');
+//   console.log('Script file is loaded !');
+//   console.log('Script file is loaded !');
+//   console.log('Script file is loaded !');
+//   console.log('Script file is loaded !');
+// console.timeEnd('Hello');
+
+
+//VAR
+
+// var name = "Akash Chowdhury 1";
+// console.log(name);
+// var name = "Akash Chowdhury 2";
+// console.log(name);
+// var cool;
+// console.log(cool);
+
+// LET
+
+// let name;
+// name = "Akash";
+// console.log(name);
+// name = "Akash 2";
+// console.log(name);
+
+//CONST
+
+// const name = "name";
+// console.log(name);
+// we cannot initialize const variable
+
+// const person = {
+//   name:'akash',
+//   age:30
+// }
+// console.log(person);
+// person.name = 'Sara';
+// person.age = 28;
+// console.log(person);
+
+// const numbers = [1,2,3,4,5];
+// console.log(numbers);
+// numbers.push(6);
+// console.log(numbers);
+
+// Data Types
+
+// Primitive Types
+//  const name = 'Johnm Deo';
+//  console.log(name);
+//  console.log(typeof name);
+
+//  const number = 56;
+//  console.log(typeof number);
+
+//  const hasKids = true;
+//  console.log(typeof hasKids);
+
+//  const car = null;
+//  console.log(typeof car);
+
+//  let test;
+//  console.log(typeof test);
+
+//  const sym = Symbol();
+//  console.log(typeof sym);
+
+ // Refernce Types
+
+// const hobbies = ['movies', 'cricket'];
+// console.log(typeof hobbies);
+
+// const address = {
+//   address: 'Dankuni',
+//   street: 'TN Mukherjee Road'
+// };
+// console.log(typeof address);
+
+// const today = new Date();
+// console.log(typeof today);
+// console.log(today);
+
+
+// Type Conversion
+
+// let val;
+
+// Number to String
+
+// val = String(65);
+// val = String(4+4);
+
+// Boolean to String
+
+// val = String(true);
+
+// Date to String
+
+// val = String(new Date());
+
+// Array to String
+
+// val = String ([1,2,3])
+
+// toString()
+
+// val = (5).toString();
+// val = (true).toString();
+
+// Strings to Number
+
+// val = Number('5');
+// val = Number(true);
+// val = Number(false);
+// val = Number(null);
+// val = Number('Hello');
+// val = Number([1,2,3]);
+
+// parse conversion
+
+// val = parseInt('100');
+// val = parseFloat('100.2');
+
+// Output
+// console.log(val);
+// console.log(typeof val);
+// console.log(val.length);
+// console.log(val.toFixed(2));
+
+// const val1 = String(5);
+// const val2 = 6;
+// const sum = Number(val1 + val2);
+// console.log(sum);
+// console.log(typeof sum);
+
+
