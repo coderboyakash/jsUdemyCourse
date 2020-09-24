fcb14255b2270e5f2feb712db6748e3813395890
coderboyakash <akashchowdhury864@gmail.com>
2020-09-24 21:56:19 +0530

    advanced loops

--- app.js
+++ app.js
@@ -468,33 +468,34 @@
 //   console.log(i);
 // }
 
-const cars = ['Apple', 'Samsung', 'Redmi', 'Nokia'];
+// const cars = ['Apple', 'Samsung', 'Redmi', 'Nokia'];
 
-cars.forEach(function(car, index, array){
-  console.log(`${index} : ${car}`);
-  console.log(`${array}`);
-})
+// cars.forEach(function(car, index, array){
+//   console.log(`${index} : ${car}`);
+//   console.log(`${array}`);
+// })
 
 // Mapping
 
-const users = [
-  {id:1, name:'John'},
-  {id:2, name:'Sharah'},
-  {id:3, name:'Carol'},
-  {id:4, name:'Steve'},
-];
+// const users = [
+//   {id:1, name:'John'},
+//   {id:2, name:'Sharah'},
+//   {id:3, name:'Carol'},
+//   {id:4, name:'Steve'},
+// ];
 
-const ids = users.map(function(user){
-  return user.id;
-})
-console.log(ids);
+// const ids = users.map(function(user){
+//   return user.id;
+// })
+// console.log(ids);
 
-const user = {
-  fname: 'Steve',
-  lname: 'Deo',
-  age: 30,
-}
+// const user = {
+//   fname: 'Steve',
+//   lname: 'Deo',
+//   age: 30,
+// }
 
-for(let x in user){
-  console.log(`${x} : ${user[x]}`)
-}
+// for(let x in user){
+//   console.log(`${x} : ${user[x]}`)
+// }
+
